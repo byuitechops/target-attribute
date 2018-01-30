34955f0594f8643d7f8a288530a45d7b686efa10
Danverde <gre12014@byui.edu>
2018-01-29 17:17:59 -0700

    updated logs. cleaned out some lint

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 /*eslint-env node, es6*/
-/*eslint no-unused-vars:1*/
-/*eslint no-console:0*/
 
-/* The module chacks all html files for external links <a href="!...buyi.edu..." and makes sure
+/* The module checks all html files for external links <a href="!...buyi.edu..." and makes sure
    the all open in the new widow (target="_blank") */
 
 module.exports = (course, stepCallback) => {
     /* Create the module report so that we can access it later as needed.
     This MUST be done at the beginning of each child module. */
-    course.addModuleReport('target-attribute');
     course.newInfo('Changed Link Target Attributes', []);
 
     // #1 -- get the course content and put it into array
@@ -19,16 +16,16 @@ module.exports = (course, stepCallback) => {
         var links = $('a').get();
         if (links.length != 0) {
             // #3 -- using cheerio, filter out all the external links
-            links = links.filter(function (element, index) {
+            links = links.filter(function (element) {
                 if ($(element).attr('href')) {
                     return ($(element).attr('href').includes('http') ||
-                        $(element).attr('href').includes('www')) &&
+                            $(element).attr('href').includes('www')) &&
                         !$(element).attr('href').includes('byui.brightspace') &&
                         !$(element).attr('href').includes('pathway.brightspace');
                 } else {
                     return false;
                 }
-                
+
             });
             // #4 -- check out all the target attributes, -->
             links.forEach(function (link) {
@@ -40,19 +37,16 @@ module.exports = (course, stepCallback) => {
                             name: file.name,
                             href: link.attribs.href
                         });
-                        course.success(
-                            'target-attribute',
-                            `${file.name} | "${link.attribs.href}" has been made to open in a new tab`
-                        );
+                        course.log('External links set to open in a new Tab', {
+                            'File Name': file.name,
+                            'Href': link.attribs.href
+                        });
                     }
                 }
             });
         }
     });
 
-    course.success(
-        'target-attribute',
-        `All external links have been made to open in a new tab`
-    );
+    course.message('All external links have been made to open in a new tab');
     stepCallback(null, course);
 };
